54cc339157374a957f7c0debaa524bc621bd0c3a
Wathrak <wathrak1@gmail.com>
2024-11-20 23:34:56 +0700

    add views folder + routers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
-  <div>Hello World</div>
+  <header>
+    <div>
+      <nav class="nav-links">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/browse">Browse</RouterLink>
+        <RouterLink to="/product">Product</RouterLink>
+        <RouterLink to="/">PTES SART</RouterLink>
+        <input type="text" placeholder="Search" />
+        <RouterLink to="/login">LOGIN</RouterLink>
+        <RouterLink to="/register">REGISTER</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<script setup></script>
+<script>
+export default {}
+</script>
 
-<style></style>
+<style>
+.nav-links a {
+  text-decoration: none;
+  color: inherit;
+  margin: 0 10px;
+}
+
+.nav-links {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  background-color: #f8f9fa;
+}
+
+.search-input {
+  margin: 0 10px;
+  padding: 5px 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
-import App from '@/App.vue'
+import BrowsePage from '@/views/BrowsePage.vue'
+import HomePage from '../views/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import ProductPage from '@/views/ProductPage.vue'
+import CheckoutPage from '@/views/CheckoutPage.vue'
+import LoginPage from '@/views/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +11,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: App,
+      component: HomePage,
+    },
+    {
+      path: '/browse',
+      name: 'browse',
+      component: BrowsePage,
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: ProductPage,
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: CheckoutPage,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
     },
   ],
 })
