7d7432316f867cd28f78a56af5de226c9f0af4cf
Wathrak <wathrak1@gmail.com>
2024-12-02 22:14:19 +0700

    update Home Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Browse from '@/views/Browse.vue'
-import HomePage from '@/views/HomePage.vue'
+import Login from '@/views/Login.vue'
+import Product from '@/views/Product.vue'
+import Checkout from '@/views/Checkout.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/browse', name: 'Browse', component: Browse },
+  { path: '/login', name: 'login', component: Login },
+  { path: '/product', name: 'Product', component: Product },
+  { path: '/checkout', name: 'Checkout', component: Checkout },
 ]
 
 const router = createRouter({
