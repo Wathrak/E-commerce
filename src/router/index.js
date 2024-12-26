44180605407bb1488dcaebc0ad9e9c879aa7e78e
SunLeang123 <leangkiyan2020@gmail.com>
2024-12-26 11:43:51 +0700

    modify index in route for solve conflict

--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,19 @@ import Login from '@/views/Login.vue'
 import Product from '@/views/Product.vue'
 import Checkout from '@/views/Checkout.vue'
 import Register from '@/views/Register.vue'
-
+import Cart from '@/views/Cart.vue'
+import Wishlist from '@/views/Wishlist.vue'
 
 const routes = [
-  { path: '/register', name: 'Register', component: Register },
   { path: '/', name: 'Home', component: Home },
+  { path: '/browse', name: 'Browse', component: Browse },
   { path: '/login', name: 'login', component: Login },
   { path: '/product', name: 'Product', component: Product },
   { path: '/checkout', name: 'Checkout', component: Checkout },
-  {
-    path: "/browse/:category",
-    name: "Browse",
-    component: Browse,
-  },
-  {
-    path: "/browse",
-    redirect: "/browse/walldecor",
-  },
+  { path: '/register', name: 'Register', component: Register },
+  { path: '/cart', name: 'Cart', component: Cart },
+  { path: '/wishlist', name: 'Wishlist', component: Wishlist },
+
 ]
 
 const router = createRouter({
